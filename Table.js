330e4e55827e5a251663cba624c969a883265d20
Nanjiro <armougom.t@gmail.com>
2022-03-11 00:57:13 +0400

    dernier commit

--- Table.js
+++ Table.js
@@ -9,16 +9,16 @@ async function render() {
   let result1 = "";
   let result2 = "";
   for (pays in data) {
-    result += `<li class="list-group-item"> ${data[pays].name.official} </li>`;
+    result += `<li class="list-group-item fw-bold px-1"> ${data[pays].name.official} </li>`;
   }
   for (Zone in data) {
-    result0 += `<li class="list-group-item"> ${data[Zone].area} </li>`;
+    result0 += `<li class="list-group-item text-end pe-1"> ${data[Zone].area.toLocaleString("en-US")} </li>`;
   }
   for (Pop in data) {
-    result1 += `<li class="list-group-item"> ${data[Pop].population} </li>`;
+    result1 += `<li class="list-group-item text-end pe-1"> ${data[Pop].population.toLocaleString("en-US")} </li>`;
   }
   for (nom in data) {
-    result2 += `<li class="list-group-item"> ${data[nom].capital} </li>`;
+    result2 += `<li class="list-group-item px-1"> ${data[nom].capital} </li>`;
   }
   console.log(typeof "list");
   document.getElementById("list").innerHTML = result;
