f5e744cdc14e0d9a695397bf36d622c3c6930c00
Nanjiro974 <armougom.t@gmail.com>
2022-03-10 15:46:55 +0400

    commit

--- Table.js
+++ Table.js
@@ -4,11 +4,26 @@ async function render() {
 
   console.log(data);
 
-  let result = [""];
-
+  let result = "";
+  let result0 = "";
+  let result1 = "";
+  let result2 = "";
   for (pays in data) {
-    result += `<table> ${data[pays].name.official} </table>`;
+    result += `<li class="list-group-item"> ${data[pays].name.official} </li>`;
   }
-  document.getElementById("table").innerHTML = result;
+  for (Zone in data) {
+    result0 += `<li class="list-group-item"> ${data[Zone].area} </li>`;
+  }
+  for (Pop in data) {
+    result1 += `<li class="list-group-item"> ${data[Pop].population} </li>`;
+  }
+  for (nom in data) {
+    result2 += `<li class="list-group-item"> ${data[nom].capital} </li>`;
+  }
+  console.log(typeof "list");
+  document.getElementById("list").innerHTML = result;
+  document.getElementById("list0").innerHTML = result0;
+  document.getElementById("list1").innerHTML = result1;
+  document.getElementById("list2").innerHTML = result2;
 }
 render();
